c4deda4e8c685b96788080679f4bf056931c5704
DaviMontarroyos <montarroyosdavi7@gmail.com>
2025-05-31 12:43:33 -0300

    Update script-01.js

--- script-01.js
+++ script-01.js
@@ -1,3 +1,10 @@
 function dataHora(){
         document.getElementById('demo').innerHTML = Date();
 }
+function dataHora(id){
+        document.getElementById(id).innerHTML = Date() + ", Legal";
+
+}
+function msg(id, msg){
+        document.getElementById(d).innerHTML = msg;
+}
